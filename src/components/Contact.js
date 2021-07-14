ef26c5a27df2e87472c9b0e3ac72e9f55e3c471f
BRANDON <brundon.cyx@gmail.com>
2021-07-14 23:07:55 +0800

    Completed form component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,127 @@
-import React from "react";
+import React, { useState } from "react";
+import { send } from 'emailjs-com';
 
 export default function Contact() {
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    from_email: '',
+    message: '',
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_48rx56i',
+      'template_ykkkfxg',
+      toSend,
+      'user_hFSEDrbIoAhY6BS2Vmw5z'
+    )
+      .then((response) => {
+        alert("Message sent!");
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        alert(err);
+        console.log('FAILED...', err);
+      });
+      setToSend({ 
+        from_name: '',
+        from_email: '',
+        message: '',
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
   return (
     <section id="contact" className="relative">
       <div className="container items-center px-5 py-10 mt-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        {/* <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+        <div class="lg:w-1/2 md:w-1/2 ml-6 mr-auto px-4 w-full md:py-8 mt-8 md:mt-0">
+          <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-green-600">
+            <img alt="..." src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=700&amp;q=80" class="w-full align-middle rounded-t-lg"></img>
+            <blockquote class="relative p-8 mb-4">
+              <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95" class="absolute left-0 w-full block" style={{height:'95px',top:'-94px'}}><polygon points="-30,95 583,95 583,65" class="text-green-600 fill-current"></polygon></svg>
+          <h4 class="text-xl font-bold text-white">More about me</h4>
+          <p class="text-md font-light mt-2 text-white">
+            In an ideal day, I would like to work on building applications
+            that are usable and scalable. When I'm not programming, I 
+            would be cycling, catching up on financial news and spending
+            time with my loved ones.
+          </p>
+          </blockquote>
+          </div>
+          </div>
+        <form
+          onSubmit={onSubmit}
+          className="lg:w-2/5 md:w-1/2 mr-6 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Connect With Me!
+          </h2>
+          <p className="leading-relaxed mb-5">
+            I'm actively looking for members to join me on hackathons and startathons!
+            Open to new working styles and team structures! Do drop me an email or join
+            <a href="https://t.me/SandboxSG" className="text-blue-400 hover:text-blue-600"> SandBox SG </a>
+            for hackathon openings!
+          </p>
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="from_name"
+              placeholder='Your name'
+              value={toSend.from_name}
+              onChange={handleChange}
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="from_email"
+              placeholder='Your email'
+              value={toSend.from_email}
+              onChange={handleChange}
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-400">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              placeholder='Your message'
+              value={toSend.message}
+              onChange={handleChange}
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-green-600 border-0 py-2 px-6 focus:outline-none hover:bg-green-700 rounded text-lg">
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+
+// Map Implementation
+{/* <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
             width="100%"
             height="100%"
@@ -40,22 +157,9 @@ export default function Contact() {
             </div>
           </div>
         </div> */}
-        <div class="lg:w-1/2 md:w-1/2 ml-6 mr-auto px-4 w-full md:py-8 mt-8 md:mt-0">
-          <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-green-600">
-            <img alt="..." src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=700&amp;q=80" class="w-full align-middle rounded-t-lg"></img>
-            <blockquote class="relative p-8 mb-4">
-              <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95" class="absolute left-0 w-full block" style={{height:'95px',top:'-94px'}}><polygon points="-30,95 583,95 583,65" class="text-green-600 fill-current"></polygon></svg>
-          <h4 class="text-xl font-bold text-white">More about me</h4>
-          <p class="text-md font-light mt-2 text-white">
-            In an ideal day, I would like to work on building applications
-            that are usable and scalable. When I'm not programming, I 
-            would be cycling, catching up on financial news and spending
-            time with my loved ones.
-          </p>
-          </blockquote>
-          </div>
-          </div>
-        <form
+
+// Original Form Implementation
+{/* <form
           netlify
           name="contact"
           className="lg:w-2/5 md:w-1/2 mr-6 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
@@ -105,8 +209,4 @@ export default function Contact() {
             className="text-white bg-green-600 border-0 py-2 px-6 focus:outline-none hover:bg-green-700 rounded text-lg">
             Submit
           </button>
-        </form>
-      </div>
-    </section>
-  );
-}
+        </form> */}
